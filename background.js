26f0097b9e2330dd3596ecd6682b98a131c2894e
Anjaan <mukeshsoni73362@gmail.com>
2022-02-25 18:02:36 +0530

    initial commit

---
+++ background.js
@@ -0,0 +1,8 @@
+chrome.runtime.onInstalled.addListener(() => {
+    console.log('Extension installed');
+})
+
+chrome.bookmarks.onCreated.addListener(() => {
+
+    alert('Bookmark created');
+})
---
+++ js/main.js
@@ -0,0 +1,16 @@
+document.getElementById("username1").value = 'admin';
+document.getElementById("psd1").value = 'Anjaan';
+document.getElementById("verification_code").value = document.getElementById("check_code").value;
+
+
+
+// let val = document.getElementById("username1");
+// val.addEventListener('input', function (evt) {
+//     document.getElementById("psd1").value = this.value;
+// })
+// console.log(chrome.storage.local.get("ftth_password"));
+
+// console.log(chrome.storage.local.get(["ftth_password"]));
+    // setTimeout(function () {
+    //     document.getElementsByTagName('login')[0].submit();
+    // }, 2000);
